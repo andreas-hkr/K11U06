436231660fcbc5ea7a108bea63572aa406b66f21
Andreas Nilsson <andreas.nilsson@hkr.se>
2023-03-18 11:01:55 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        // Skriv två metoder, sum och kvadSum, som returnerar summan respektive summan av kvadraterna
+
+
+    }
+}
