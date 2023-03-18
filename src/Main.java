2abcf6e86a7e55dda0dd122205b51531e8850482
Andreas Nilsson <andreas.nilsson@hkr.se>
2023-03-18 11:07:24 +0100

    Solved

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,31 @@
 public class Main {
+    static double sum(double[] arr) {
+        double total = 0;
+
+        for(int i=0; i < arr.length; i++) {
+            total += arr[i];
+        }
+
+        return total;
+    }
+
+    static double kvadSum(double[] arr) {
+        double total = 0;
+
+        for(int i=0; i < arr.length; i++) {
+            total += arr[i] * arr[i];
+        }
+
+        return total;
+    }
+
+
     public static void main(String[] args) {
         // Skriv två metoder, sum och kvadSum, som returnerar summan respektive summan av kvadraterna
 
+        double[] arr = {2.5, 1.5, 3.0};
 
+        System.out.println("sum     = " + sum(arr));
+        System.out.println("kvadSum = " + kvadSum(arr));
     }
 }
